de09a909f0bb00063d9f706a09180eaf77d0dfec
Ubuntu <ubuntu@ip-172-31-37-224.ec2.internal>
2014-08-07 19:03:54 +0000

    Changing port to 8888

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var express  = require('express');
 var sys		= require('sys');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var port     = process.env.PORT || 8888;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash 	 = require('connect-flash');
